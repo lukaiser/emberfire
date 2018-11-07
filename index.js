d5223cbc3754b831a87bb3c9e6da665ec169f9a7
Lukas <Kaiser>
2018-11-07 18:41:12 +0100

    Autoimport

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('node_modules/firebase/firebase-app.js');
+    //app.import('node_modules/firebase/firebase-app.js');
     //app.import('node_modules/firebase/firebase-auth.js');
     //app.import('node_modules/firebase/firebase-database.js');
   },
